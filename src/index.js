55bc05d087f5201623cc7f24a4d5ec78be16bd4c
David Puerto <me@davidpuerto.com>
2017-12-19 15:15:53 -0800

    adding redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import rootReducer from "./reducers";
 import App from "./components/App/App";
 import Stack from "./components/Stack/Stack";
+import { setStack } from "./actions";
 import "./index.css";
+
+const store = createStore(rootReducer);
+store.subscribe(() => console.log("store", store.getState()));
+store.dispatch(setStack({ id: 0, title: "example", cards: [] }));
 
 ReactDOM.render(
   <BrowserRouter>
