9fc7108b00000b931a07b9cb3a639e78f9a23696
David Puerto <me@davidpuerto.com>
2017-12-19 22:51:25 -0800

    mapping through props and destructuring syntax

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+
 import "./Stack.css";
 
 class Stack extends Component {
   render() {
+    //console.log("stack props", this.props);
+    const { title, cards } = this.props.stack;
     return (
       <div>
         <Link to="/">Home</Link>
-        <h3>Stack</h3>
+        <h3>{title}</h3>
+        <hr />
+        {cards.map(card => {
+          return <div key={card.id}>{card.prompt}</div>;
+        })}
       </div>
     );
   }
 }
 
-export default Stack;
+function mapStateToProps(state) {
+  return {
+    stack: state
+  };
+}
+
+export default connect(mapStateToProps, null)(Stack);
