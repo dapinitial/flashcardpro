75893ff163b5db30d4b35c1119772386046b50bd
David Puerto <me@davidpuerto.com>
2017-12-21 22:59:53 -0800

    refactoring to stateless, functional components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom";
 import StackList from "../StackList/StackList";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Flashcard Pro</h2>
-        <StackList />
-        <hr />
-        <Link to="/stack_form">
-          <h4>Create a new stack</h4>
-        </Link>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h2>Flashcard Pro</h2>
+      <StackList />
+      <hr />
+      <Link to="/stack_form">
+        <h4>Create a new stack</h4>
+      </Link>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -5,24 +5,20 @@ import Card from "../Card/Card";
 
 import "./Stack.css";
 
-export class Stack extends Component {
-  render() {
-    console.log("stack props", this.props);
-    const { title, cards } = this.props.stack;
-    return (
-      <div>
-        <Link to="/" className="link-home">
-          <h4>Home</h4>
-        </Link>
-        <h3>{title}</h3>
-        <br />
-        {cards.map(card => {
-          return <Card key={card.id} card={card} />;
-        })}
-      </div>
-    );
-  }
-}
+export const Stack = ({ stack: { title, cards } }) => {
+  return (
+    <div>
+      <Link to="/" className="link-home">
+        <h4>Home</h4>
+      </Link>
+      <h3>{title}</h3>
+      <br />
+      {cards.map(card => {
+        return <Card key={card.id} card={card} />;
+      })}
+    </div>
+  );
+};
 
 function mapStateToProps(state) {
   console.log("state", state);
